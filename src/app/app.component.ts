4081f7f038fa4d234820d33250e820865523389c
Wira <wira000017@gmail.com>
2022-03-27 00:19:30 +0800

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KatexOptions } from 'ng-katex';
+import pdfMake from 'pdfmake/build/pdfMake'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit {
+  equation: string = 'x=\\frac{-b\\pm\\sqrt{b^2-4ac}}{2a}';
+  options: KatexOptions = {
+    displayMode: true,
+  };
+
+  ngOnInit(): void {
+      
+  }
   title = 'math-project';
+
+  generatePDF(){
+    var dd = {
+      content: [
+        'fomrula is here'
+      ]
+    }
+
+    const pdfObj = pdfMake.createPdf(dd).open();
+  }
+
+  onChangeFormula(text: string){
+    this.equation = text;
+  }
 }
